b27961527a3c8a14d8183d863a7da0b984cc9e7d
Urmasa <n.goltsev@g.nsu.ru>
2023-07-09 10:00:16 +0700

    Rebuild

--- src/test/test_example.py
+++ src/test/test_example.py
@@ -8,25 +8,30 @@ from Lamel_window.proccesing import Filter_for_data
 
 class TestFluidCube(unittest.TestCase):
     def testFluid(self):
+        result = False
+        try:
+            model = moduls.read_for_arr(np.array([]))
+            self.AssertModelStep(model)
+        except:
+            result = True
+            model = moduls.read_for_arr(np.array([1, 1, 1, 1]))
+            self.AssertModelStep(model)
+            model = moduls.read_for_arr(np.array([1, -9, 1, 100]))
+            self.AssertModelStep(model)
 
-        model = moduls.read_for_arr(np.array([]))
-        self.AssertModelStep(model)
-
-        model = moduls.read_for_arr(np.array([1, 1, 1, 1]))
-        self.AssertModelStep(model)
-
-        return True
+        return result
 
     def CriationAssert(self, model):
+        result = True
         try:
             local_m = Filter_for_data()
         except:
             print("An error occurred during the step function!")
-            return False
+            result = False
         local_m = Filter_for_data()
         model = moduls.read_for_arr(np.array([1, 1, 1, 1]))
         self.AssertModelStep(model)
-        return True
+        return result
 
 def run_tests():
     unittest.main()
